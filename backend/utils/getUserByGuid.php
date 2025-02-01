77dd61c75c1aa339dbf3e7944a0b3e776578253e
Nuruddin Tipu <nuruddintipu.connect@gmail.com>
2025-02-02 04:28:36 +0600

    refactored userService

---
+++ backend/utils/getUserByGuid.php
@@ -0,0 +1,11 @@
+<?php
+function getUserByUuid($guid) {
+    $users = include '../api/userData.php';
+    $users = $users['users'];
+    foreach ($users as $user) {
+        if ($user['guid'] == $guid) {
+            return $user;
+        }
+    }
+    return null;
+}
