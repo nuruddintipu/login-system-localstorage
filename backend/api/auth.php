d02ce59b4eaf8a5a80f74472dada237b5fa22814
Nuruddin Tipu <nuruddintipu.connect@gmail.com>
2025-02-02 03:53:21 +0600

    separated headers and options methods approval for reuse

--- backend/api/auth.php
+++ backend/api/auth.php
@@ -1,14 +1,8 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:3000");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Credentials: true");
+include '../utils/headers.php';
 
+include '../utils/approveOptionsMethod.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
 
 $data = json_decode(file_get_contents('php://input'), true);
 
