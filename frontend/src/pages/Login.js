753dce310326759cfe3fce2544579f72806a0f2e
Nuruddin Tipu <nuruddintipu.connect@gmail.com>
2025-02-02 02:11:32 +0600

    added page template

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -5,6 +5,7 @@ import {getRoutePath, NamedLink} from "../routes/NamedLink";
 import handleInputChange from "../utils/handleInputChange";
 import {useNavigate} from "react-router-dom";
 import {loginUser} from "../services/authRequest";
+import PageTemplate from "../components/PageTemplate";
 
 function Login() {
     const userRef = useRef();
@@ -43,49 +44,46 @@ function Login() {
     }, [user])
 
     return (
-        <div className="login-wrapper">
-            <div className="login-form-container">
-                <h2 className="login-title">Login</h2>
-                {responseMessage && <div className="alert alert-danger">{responseMessage}</div>}
-                <Form className="login-form" onSubmit={handleSubmit}>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label column="lg">Email Address</Form.Label>
-                        <Form.Control
-                            type="email"
-                            ref={userRef}
-                            placeholder="Enter email"
-                            onChange={(event) => handleInputChange("email", event.target.value, setUser)}
-                            value={user.email}
-                            isInvalid={!!errors.email}
-                        />
+        <PageTemplate pageTitle="Login">
+            {responseMessage && <div className="alert alert-danger">{responseMessage}</div>}
+            <Form className="login-form" onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label column="lg">Email Address</Form.Label>
+                    <Form.Control
+                        type="email"
+                        ref={userRef}
+                        placeholder="Enter email"
+                        onChange={(event) => handleInputChange("email", event.target.value, setUser)}
+                        value={user.email}
+                        isInvalid={!!errors.email}
+                    />
 
-                        <Form.Control.Feedback type="invalid">
-                            {errors.email}
-                        </Form.Control.Feedback>
-                    </Form.Group>
+                    <Form.Control.Feedback type="invalid">
+                        {errors.email}
+                    </Form.Control.Feedback>
+                </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label column="lg">Password</Form.Label>
-                        <Form.Control
-                            type="password"
-                            placeholder="Password"
-                            onChange={(event) => handleInputChange("password", event.target.value, setUser)}
-                            value={user.password}
-                            isInvalid={!!errors.password}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {errors.password}
-                        </Form.Control.Feedback>
-                    </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label column="lg">Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        onChange={(event) => handleInputChange("password", event.target.value, setUser)}
+                        value={user.password}
+                        isInvalid={!!errors.password}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {errors.password}
+                    </Form.Control.Feedback>
+                </Form.Group>
 
-                    <Button variant="primary" type="submit" className="login-button">Login</Button>
-                </Form>
-                <p>
-                    Need an Account?
-                    <NamedLink routeName='SIGNUP'> Sign Up</NamedLink>
-                </p>
-            </div>
-        </div>
+                <Button variant="primary" type="submit" className="login-button">Login</Button>
+            </Form>
+            <p>
+                Need an Account?
+                <NamedLink routeName='SIGNUP'> Sign Up</NamedLink>
+            </p>
+        </PageTemplate>
     );
 }
 
