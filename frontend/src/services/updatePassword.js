1b32eb2f214d11c081ac04c07415f8d6b3c1dce7
Nuruddin Tipu <nuruddintipu.connect@gmail.com>
2025-02-03 05:28:13 +0600

    refactored

--- frontend/src/services/updatePassword.js
+++ frontend/src/services/updatePassword.js
@@ -2,12 +2,11 @@ import {sendRequest} from "./sendRequest";
 
 export const updatePassword = async (user, currentPassword, newPassword, confirmPassword) => {
 
-    const response = await sendRequest('edit.php', 'PUT', {
+    return await sendRequest('edit.php', 'PUT', {
         type: 'password',
         guid: user.guid,
         currentPassword: currentPassword,
         newPassword: newPassword,
         confirmPassword: confirmPassword
     });
-    return response;
 };
